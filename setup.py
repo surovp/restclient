f7c9a0fdc79e4b1e9e4b12f9b72eb76b662047ce
Pavel Surov <surov_p@tanukitech.ru>
2023-06-12 15:42:09 +0500

    вынесен отдельно restclient

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+REQUIRES = [
+    'structlog',
+    'requests',
+    'allure-pytest',
+    'allure',
+    'curlify'
+]
+
+setup(
+    name='restclient',
+    version='0.0.1',
+    packages=['restclient'],
+    url='https://github.com/surovp/restclient.git',
+    license='MIT',
+    author='Pavel Surov',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='restclient with log and send request'
+)
